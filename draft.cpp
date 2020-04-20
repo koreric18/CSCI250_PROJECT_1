34d90475f23e540ec02f42f04bea8a064cfda995
koreric18 <30818848+koreric18@users.noreply.github.com>
2020-04-19 21:29:41 -0700

    Update draft.cpp

--- draft.cpp
+++ draft.cpp
@@ -1,96 +1,183 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
 // My parser will retrieve the commands and constructs object using Builder interface
-class parseInput { //director
-    private:
-        scriptParser* builder;
-    public: 
-        void setBuilder (scriptParser* newBuilder) {
-            builder = newBuilder;
-        }
-        void parse();     
-    }
-};
-
-void parseInput:: parse() {
-    ifstream myfile;
-    myfile.open("script1.txt");
-    string line;
-    while (!getline(myfile,line).eof()) {
-        if (line.find("mkdir")) {
-            scriptParser.makeDirectory(line);
-        }
-        else if (line.find("create")) {
-            scriptParser.createFile(line);
-        }
-        else if (line.find("cd")) {
-            scriptParser.changeDirectory(line);
-        }
-        else if (line.find("del")) {
-            scriptParser.deletefd(line);
-        }
-        else if (line.find("size")) {
-            scriptParser.size(line);
-        }
-        else if (line.find("ls")) {
-            scriptParser.ls(line);
-        }
-    }
-}
-
-class scriptParser { // builder
-    string name;
-    int size;
-    virtual void makeDirectory(name);
-    virtual void createFile(name, size);
-    virtual void changeDirectory(name);
-    virtual void deletefd(name);
-    virtual void size(name);
-    virtual void list(name);
-};
-
-class fileSystem: public scriptParser {
-    public:
-        /*fileSystem() {
-
-        }*/
-        void makeDirectory(name){
-            (root->children).push_back(newNode(name));
-        }
-        /*void createFile(name, size){
-
-        }
-        void changeDirectory(name) {
-
-        }
-        void deletefd(name) {
-
-        }
-        void size(name) {
-
-        }
-        void list(name) {
-
-        }*/
-} 
 struct node {
-    string name;
+    string fname;
+    int size;
     vector<node *> children;
-    struct node *root;
 };
 
 node *newnode(string name) {
     node *tmp = new node;
-    tmp -> key = key; 
+    tmp -> fname = name; 
     return tmp;
-} 
+}
+
+class scriptParser { // builder
+    public:
+        int size;
+        virtual void extractString(string name){}
+        virtual void makeDirectory(string name){}
+        virtual void createFile(string name){} // passes the command line
+        virtual void changeDirectory(string name){}
+        virtual void deletefd(string name){}
+        virtual void fdsize(string name){}
+        virtual void list(string name){}
+    protected: 
+    node *parent;
+    node *root; // root node of the file system
+    node *curr = root; // current pointer to track where user is at
+};
+
+class fileSystem: public scriptParser {
+    public:
+        void makeDirectory(string name){
+        //    stringstream substring;
+        //    substring << name; // storing whole string into ss
+        //    string temp, tempname;
+         //   substring >> temp >> tempname;
+            cout << "hello";
+            (root->children).push_back(newnode(name));
+        }
+/*        void createFile(string name){
+            stringstream substring;
+            substring << name; // storing whole string into ss
+            string temp, tempname;
+            int mysize;   
+            substring >> temp >> tempname >> mysize;
+            (curr->children).push_back(newnode(tempname));
+            curr->size = mysize;
+        }
+        void changeDirectory(string name) { // curr pointer should only be changed here?
+            // two cases
+            stringstream substring;
+            string temp, tempname;
+            tempname = "";
+            substring << name; // storing whole string into ss
+            substring >> temp >> tempname;
+            // second case, cd ..., set curr to a child
+            if (tempname != "") {
+                for (int i = 0; i < curr->children.size(); ++i) {
+                    if (curr->children.at(i)->fname == tempname) {
+                        parent = curr;
+                        curr->children.at(i);
+                    }
+                }
+            }
+            else { // first case, cd, set the curr to the parent node.
+                curr = parent; 
+            }
+        }
+        void deletefd(string name) { // currently deletes file or directory if its in the path
+            stringstream substring;
+            string temp, tempname;
+            substring << name;
+            substring >> temp >> tempname;
+            for (int i =0; i < parent->children.size(); ++i) {
+                if (parent->children.at(i)->fname == tempname) {
+                    parent->children.erase(parent->children.begin() + i);
+                }
+            }
+        }
+        void fdsize(string name) { // still need to do for size of all files
+            stringstream substring;
+            string temp, tempname;
+            substring << name;
+            substring >> temp >> tempname;
+            for (int i =0; i < parent->children.size(); ++i) {
+                if (parent->children.at(i)->fname == tempname) {
+                    //if (parent->children.at(i).size() > 0) {
+                    //    for (parent->children)
+                    //}
+                    cout << "Size: " << parent->children.at(i)->size;
+                }
+            }
+
+        }
+        /*traversing the vector of nodes...
+        while (tempptr->name != fname) {
+            for (int i =0; i < root->children.size(); ++i) {
+
+            }
+        }
+        void list(string name) {
+            stringstream substring;
+            string temp, tempname;
+            tempname = "";
+            substring << name;
+            substring >> temp >> tempname;  
+            if (tempname == "") {
+                for (int i = 0; i < parent->children.size(); ++i){
+                    cout << parent->children.at(i)->fname << "size: " << parent->children.at(i)->size << endl;
+                }
+            }  
+        }*/
+};
+
+class Director { //director
+       scriptParser builder;
+    public: 
+        void setBuilder (scriptParser newBuilder) {
+            builder = newBuilder;
+        }
+        void parse();     
+};
+
+void Director:: parse() {
+    ifstream myfile;
+    string line;
+    stringstream substring;
+    string command, tempname;
+    myfile.open("script1.txt");
+    if (myfile) {
+        cout<< "yellow";
+        while (getline(myfile,line)){//.eof()) {
+            substring << line;
+            substring >> command >> tempname;
+            if (command == "mkdir") {
+                cout << tempname << endl;
+                builder.makeDirectory(tempname);
+            }
+        /*    else if (line.find("create")) {
+                builder->createFile(line);
+            }
+            else if (line.find("cd")) {
+                builder->changeDirectory(line);
+            }
+            else if (line.find("del")) {
+                builder->deletefd(line);
+            }
+            else if (line.find("size")) {
+                builder->fdsize(line);
+            }
+            else if (line.find("ls")) {
+                builder->list(line);
+            }*/
+        }
+        myfile.close();
+    }
+}
 
 int main() {
 //singleton, should access instance of class through its pointer and not the actual instance
-    parseInput parser;
-    parser.parse();
+    
+    // Director control
+    Director dir;
+
+    //Concrete Builder 
+    fileSystem myfiles;
+
+    dir.setBuilder(myfiles);
+    dir.parse();
+
+
+    //Build my file system...
+    cout << "Your directory " << endl;
+    return 0;
 }
