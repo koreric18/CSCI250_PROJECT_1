d7b9723a88e26a8da5cfc404a83d96f17dfe360e
koreric18 <30818848+koreric18@users.noreply.github.com>
2020-04-21 15:06:43 -0700

    Update draft.cpp

--- draft.cpp
+++ draft.cpp
@@ -9,110 +9,99 @@ using namespace std;
 // My parser will retrieve the commands and constructs object using Builder interface
 struct node {
     string fname;
-    int size;
+    int fsize;
     vector<node *> children;
+    struct node *parent;
 };
 
-node *newnode(string name) {
-    node *tmp = new node;
+struct node *newdirectory(string name) {
+    struct node *tmp = new struct node(); // declaring and allocating new file
     tmp -> fname = name; 
-    return tmp;
+    tmp -> parent = NULL;
+    return (tmp);
 }
 
+struct node *newfile(string name, int size) {
+    struct node *tmp = new struct node();
+    tmp -> fname = name;
+    tmp -> fsize = size;
+    return (tmp);
+}
 class scriptParser { // builder
     public:
         int size;
         virtual void extractString(string name){}
         virtual void makeDirectory(string name){}
-        virtual void createFile(string name){} // passes the command line
+        virtual void createFile(string name, int filesize){}
         virtual void changeDirectory(string name){}
         virtual void deletefd(string name){}
         virtual void fdsize(string name){}
         virtual void list(string name){}
+        struct node *root = newdirectory("root");
     protected: 
-    node *parent;
-    node *root; // root node of the file system
     node *curr = root; // current pointer to track where user is at
 };
 
 class fileSystem: public scriptParser {
     public:
         void makeDirectory(string name){
-        //    stringstream substring;
-        //    substring << name; // storing whole string into ss
-        //    string temp, tempname;
-         //   substring >> temp >> tempname;
-            cout << "hello";
-            (root->children).push_back(newnode(name));
-        }
-/*        void createFile(string name){
-            stringstream substring;
-            substring << name; // storing whole string into ss
-            string temp, tempname;
-            int mysize;   
-            substring >> temp >> tempname >> mysize;
-            (curr->children).push_back(newnode(tempname));
-            curr->size = mysize;
-        }
-        void changeDirectory(string name) { // curr pointer should only be changed here?
-            // two cases
-            stringstream substring;
-            string temp, tempname;
-            tempname = "";
-            substring << name; // storing whole string into ss
-            substring >> temp >> tempname;
-            // second case, cd ..., set curr to a child
-            if (tempname != "") {
+            (curr->children).push_back(newdirectory(name));
+            if (curr == root) {
                 for (int i = 0; i < curr->children.size(); ++i) {
-                    if (curr->children.at(i)->fname == tempname) {
-                        parent = curr;
+                    curr->children.at
+                }
+            }
+        }
+        void createFile(string name, int filesize){
+            (curr->children).push_back(newfile(name, filesize));
+            for (int i = 0; i < curr->children.size(); ++i) {
+                cout <<curr->children.at(i)->fname << " size: " << curr->children.at(i)-> fsize << endl;
+            }
+
+        }
+        void changeDirectory(string name) { 
+            // second case, cd ..., set curr to a child
+            if (name != "") {
+                for (int i =0; i < curr->children.size(); ++i) {
+                    if (curr->children.at(i)->fname == name) {
                         curr->children.at(i);
+                        curr->children.at(i)->parent = curr;
                     }
                 }
             }
-            else { // first case, cd, set the curr to the parent node.
-                curr = parent; 
+            else { // going back to parents directory
+                for (int i =0; i < curr->children.size(); ++i) {
+                    if (curr->children.at(i)->fsize == 0) {
+                        curr = curr->children.at(i)->parent;
+                    }
+                };
+                
+                // set parent to grandparent
+                
             }
         }
+/*      CSCI           PHYSICS
+        150 250         200
+        2020
         void deletefd(string name) { // currently deletes file or directory if its in the path
-            stringstream substring;
-            string temp, tempname;
-            substring << name;
-            substring >> temp >> tempname;
             for (int i =0; i < parent->children.size(); ++i) {
-                if (parent->children.at(i)->fname == tempname) {
+                if (parent->children.at(i)->fname == name) {
                     parent->children.erase(parent->children.begin() + i);
                 }
             }
         }
         void fdsize(string name) { // still need to do for size of all files
-            stringstream substring;
-            string temp, tempname;
-            substring << name;
-            substring >> temp >> tempname;
             for (int i =0; i < parent->children.size(); ++i) {
-                if (parent->children.at(i)->fname == tempname) {
+                if (parent->children.at(i)->fname == name) {
                     //if (parent->children.at(i).size() > 0) {
                     //    for (parent->children)
                     //}
                     cout << "Size: " << parent->children.at(i)->size;
                 }
             }
-
         }
-        /*traversing the vector of nodes...
-        while (tempptr->name != fname) {
-            for (int i =0; i < root->children.size(); ++i) {
-
-            }
-        }
-        void list(string name) {
-            stringstream substring;
-            string temp, tempname;
-            tempname = "";
-            substring << name;
-            substring >> temp >> tempname;  
-            if (tempname == "") {
+        void list(string name) { 
+            if (name == "") {
                 for (int i = 0; i < parent->children.size(); ++i){
                     cout << parent->children.at(i)->fname << "size: " << parent->children.at(i)->size << endl;
                 }
@@ -121,48 +110,46 @@ class fileSystem: public scriptParser {
 };
 
 class Director { //director
-       scriptParser builder;
+       scriptParser *builder;
     public: 
-        void setBuilder (scriptParser newBuilder) {
+        void setBuilder (scriptParser *newBuilder) {
             builder = newBuilder;
+            ifstream myfile;
+            string line;
+            stringstream substring;
+            string command, tempname;
+            myfile.open("script1.txt");
+            if (myfile) {
+                cout<< "File is being read." << endl;
+                while (getline(myfile,line)){//.eof()) {
+                    substring.clear();
+                    substring << line;
+                    substring >> command >> tempname;
+                    if (command == "mkdir") {
+                        builder->makeDirectory(tempname);
+                    }
+                    else if (command == "create") {
+                        int filesize;
+                        substring >> filesize;
+                        builder->createFile(tempname, filesize);
+                    }
+                    else if (command == "cd") {
+                        builder->changeDirectory(tempname);
+                    }
+                    else if (command == "del") {
+                        builder->deletefd(tempname);
+                    }
+                    else if (command == "size") {
+                        builder->fdsize(tempname);
+                    }
+                    else if (line.find("ls")) {
+                        builder->list(tempname);
+                    }
+                }
+            myfile.close();
+            }
         }
-        void parse();     
 };
-
-void Director:: parse() {
-    ifstream myfile;
-    string line;
-    stringstream substring;
-    string command, tempname;
-    myfile.open("script1.txt");
-    if (myfile) {
-        cout<< "yellow";
-        while (getline(myfile,line)){//.eof()) {
-            substring << line;
-            substring >> command >> tempname;
-            if (command == "mkdir") {
-                cout << tempname << endl;
-                builder.makeDirectory(tempname);
-            }
-        /*    else if (line.find("create")) {
-                builder->createFile(line);
-            }
-            else if (line.find("cd")) {
-                builder->changeDirectory(line);
-            }
-            else if (line.find("del")) {
-                builder->deletefd(line);
-            }
-            else if (line.find("size")) {
-                builder->fdsize(line);
-            }
-            else if (line.find("ls")) {
-                builder->list(line);
-            }*/
-        }
-        myfile.close();
-    }
-}
 
 int main() {
 //singleton, should access instance of class through its pointer and not the actual instance
@@ -173,9 +160,7 @@ int main() {
     //Concrete Builder 
     fileSystem myfiles;
 
-    dir.setBuilder(myfiles);
-    dir.parse();
-
+    dir.setBuilder(&myfiles);
 
     //Build my file system...
     cout << "Your directory " << endl;
